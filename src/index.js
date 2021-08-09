8dd37d262fb92cb39b524a864cf52ce9eeacee31
Nor10ava <anastasiatasyarom@gmail.com>
2021-08-09 21:42:10 +0100

    made the grid

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './menu';
+import './styles.css';
+import menuTemplate from './templates/menu-template.hbs';
+import menuList from './menu.json';
+
+console.log(menuTemplate);
+console.log(menuList);
+
+const menuGrid = document.querySelector('.js-menu');
+const markup = menuList.map(menuTemplate).join('');
+menuGrid.insertAdjacentHTML('beforeend', markup);
