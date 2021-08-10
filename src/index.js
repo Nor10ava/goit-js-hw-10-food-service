2bd64e33caae050b794b7dc1f9b34a76d61b02ee
Nor10ava <anastasiatasyarom@gmail.com>
2021-08-10 18:10:19 +0100

    added a change theme

--- src/index.js
+++ src/index.js
@@ -8,3 +8,23 @@ console.log(menuList);
 const menuGrid = document.querySelector('.js-menu');
 const markup = menuList.map(menuTemplate).join('');
 menuGrid.insertAdjacentHTML('beforeend', markup);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const refs = {
+  toolbar: document.querySelector('#theme-switch-toggle'),
+};
+function changeTheme(e) {
+  //   e.preventDefault();
+  document.body.classList.toggle('dark-theme');
+}
+// refs.toolbar.addEventListener('change', () => {
+//   document.body.classList.toggle('dark-theme');
+// });
+
+refs.toolbar.addEventListener('change', changeTheme);
+// refs.toolbar.addEventListener('checked', changeThemeToDark);
+localStorage.setItem('theme', JSON.stringify(Theme));
+console.log(localStorage.getItem('Theme'));
